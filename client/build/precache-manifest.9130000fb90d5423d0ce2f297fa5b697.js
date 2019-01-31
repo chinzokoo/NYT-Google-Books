7d157639ef6eff531e568c21719e2f22b80d78c7
Chinzorig Boldmyagmar <chinzorig88@yahoo.com>
2019-01-30 19:48:50 -0800

    update

---
+++ client/build/precache-manifest.9130000fb90d5423d0ce2f297fa5b697.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed965c0072b3a3a01793",
+    "url": "/static/js/main.ed965c00.chunk.js"
+  },
+  {
+    "revision": "f9f3a51c331bee932f36",
+    "url": "/static/js/1.f9f3a51c.chunk.js"
+  },
+  {
+    "revision": "154c755c5f2a07c1e0d2b92d50180c94",
+    "url": "/index.html"
+  }
+];
