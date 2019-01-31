1b4015fdb30625b496bd5447b9680f63858f90ed
Chinzorig Boldmyagmar <chinzorig88@yahoo.com>
2019-01-30 19:20:51 -0800

    update server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("*", (req, res) => {
 });
 
 //connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/book");
 
 // start the API server
 app.listen(PORT, () => {
